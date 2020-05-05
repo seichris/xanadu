0c10f88be2d9bbe813565f2d25fe10119174b8aa
seichris <cerujurasa@gmail.com>
2020-05-05 18:33:36 +0200

    holyMoly it works... i can see posts with and without Metamask

--- src/App.js
+++ src/App.js
@@ -15,34 +15,109 @@ import PublicFeedComments from "./pages/PublicFeedComments";
 import AddApp from "./pages/AddApp";
 import Profile from "./pages/Profile";
 
-/*const getThreeBox = async address => {
+const getThreeBox = async address => {
   const profile = await Box.getProfile(address);
   return profile;
-};*/
+};
 
 export default class App extends Component {
   state = {
-    needToAWeb3Browser: false
+    needsAWeb3Browser: false
   };
 
   async componentDidMount() {
+    if (typeof window.ethereum == "undefined") {
+      // if there is no Metamask, show public thread.posts for this URL
+      this.setState({ needsAWeb3Browser: true });
+      const currentURL = window.location.href;
+      const cleanCurrentURL = currentURL.replace(/\//g, "_");
+      //const chris = "did:3:bafyreiefwktffgtt75edstz3kwcijfqsviv33okgciioreuzpari3lnqyu";
+      const threadCommentsThisURL = await Box.getThread(cleanCurrentURL, 'xanadu_now_sh_comments', "did:3:bafyreiefwktffgtt75edstz3kwcijfqsviv33okgciioreuzpari3lnqyu", false );
+      this.setState({ threadCommentsThisURL });
+      console.log(threadCommentsThisURL);
+    } else {
+      // if Metamask detected, then set accounts
+      window.ethereum.autoRefreshOnNetworkChange = false; //silences warning about no autofresh on network change
+      const accounts = await window.ethereum.enable();
+      this.setState({ accounts });
 
-    const currentURL = window.location.href;
-    const cleanCurrentURL = currentURL.replace(/\//g, "_");
-    const chris = "did:3:bafyreiefwktffgtt75edstz3kwcijfqsviv33okgciioreuzpari3lnqyu";
-    // check if there is a thread for this currentURL
-    const threadCommentsThisURL = await Box.getThread(cleanCurrentURL, 'xanadu_now_sh_comments', "did:3:bafyreiefwktffgtt75edstz3kwcijfqsviv33okgciioreuzpari3lnqyu", false );
-    this.setState({ threadCommentsThisURL });
+      const threeBoxProfile = await getThreeBox(this.state.accounts[0]);
+      this.setState({ threeBoxProfile });
 
-    console.log(threadCommentsThisURL);
+      //const chris = "0x336BF8be536c8C804dab7D6CA5E5076a7DE555EE";
+      const chris = "did:3:bafyreiefwktffgtt75edstz3kwcijfqsviv33okgciioreuzpari3lnqyu";
+      const box = await Box.openBox(this.state.accounts[0], window.ethereum);
+      this.setState({ box });
+      const currentURL = window.location.href;
+      const cleanCurrentURL = currentURL.replace(/\//g, "_");
+      const space = await this.state.box.openSpace(cleanCurrentURL);
+      this.setState({ space });
 
+      const threadApps = await space.joinThread("context", {
+        firstModerator: chris,
+        members: false
+      });
+      this.setState({ threadApps }, ()=>(this.getAppsThread()));
+
+      // add another thread
+      const threadComments = await space.joinThread("xanadu_now_sh_comments", {
+        firstModerator: chris,
+        members: false
+      });
+      this.setState({ threadComments }, ()=>(this.getCommentsThread()));
+      //console.log(threadComments);
     }
+  }
 
+  async getAppsThread() {
+    if (!this.state.threadApps) {
+      console.error("apps thread not in react state");
+      return;
+    }
+    const posts = await this.state.threadApps.getPosts();
+    this.setState({posts});
+    await this.state.threadApps.onUpdate(async()=> {
+      const posts = await this.state.threadApps.getPosts();
+      this.setState({posts});
+    })
+  }
+
+  async getCommentsThread() {
+    if (!this.state.threadComments) {
+      console.error("comments thread not in react state");
+      return;
+    }
+    const comments = await this.state.threadComments.getPosts();
+    this.setState({comments});
+    await this.state.threadComments.onUpdate(async()=> {
+      const comments = await this.state.threadComments.getPosts();
+      this.setState({comments});
+    })
+  }
+
+    // open Box
+
+    // open space
+
+    // open thread
+
+    // open posts
+
+    // forward all these to the components as (box, space, account, threeBoxProfile, thread, posts)
+    // trigger it in Save Post
 
 
   render() {
-    if (this.state.needToAWeb3Browser) {
-      return <h1>Please install metamask</h1>;
+
+    let postsWithOrWithoutMetamask = 0;
+    let threadWithOrWithoutMetamask = 0;
+
+    if (this.state.needsAWeb3Browser) {
+      threadWithOrWithoutMetamask = this.state.threadCommentsThisURL;
+      postsWithOrWithoutMetamask = this.state.threadCommentsThisURL;
+    } else {
+      threadWithOrWithoutMetamask = this.state.threadComments;
+      postsWithOrWithoutMetamask = this.state.comments;
     }
 
     return (
@@ -54,14 +129,14 @@ export default class App extends Component {
                 <HeroSection />
                 <HeroCommentSection
                   accounts={this.state.accounts}
-                  thread={this.state.threadCommentsThisURL}
-                  //box={this.state.box}
-                  //space={this.state.space}
+                  thread={threadWithOrWithoutMetamask}
+                  box={this.state.box}
+                  space={this.state.space}
                   threadMembers={this.state.threadMembers}
-                  posts={this.state.comments}
-                  //threeBoxProfile={this.state.threeBoxProfile}
+                  posts={postsWithOrWithoutMetamask}
+                  threeBoxProfile={this.state.threeBoxProfile}
                   //getAppsThread={this.getAppsThread.bind(this)}
-                  //getCommentsThread={this.getCommentsThread.bind(this)}
+                  getCommentsThread={this.getCommentsThread.bind(this)}
                   usersAddress={
                     this.state.accounts ? this.state.accounts[0] : null
                   }
@@ -72,7 +147,7 @@ export default class App extends Component {
                 <FooterSection />
               </div>
             </Route>
-            {/*<Route path="/profile">
+            <Route path="/profile">
               {this.state.space && (
                 <div className="container mx-auto px-4 flex flex-wrap justify-center">
                   <Profile
@@ -89,9 +164,9 @@ export default class App extends Component {
                   <ClimbingBoxLoader color={"blue"} />
                 </div>
               )}
-            </Route>*/}
+            </Route>
 
-            {/*<Route path="/add-application">
+            <Route path="/add-application">
               {this.state.accounts && (
                 <div className="container mx-auto px-4">
                   <AddApp
@@ -108,9 +183,9 @@ export default class App extends Component {
                 </div>
               )}
               {!this.state.accounts && <h1>Login with metamask</h1>}
-            </Route>*/}
+            </Route>
 
-            {/*<Route exact path="/public-feed">
+            <Route exact path="/public-feed">
               <div className="container mx-auto px-4">
                 <PublicFeed
                   posts={this.state.posts}
@@ -123,9 +198,9 @@ export default class App extends Component {
                 />
                 <FooterSection />
               </div>
-            </Route>*/}
+            </Route>
 
-            {/*<Route exact path="/public-feed-comments">
+            <Route exact path="/public-feed-comments">
               <div className="container mx-auto px-4">
                 <PublicFeedComments
                   posts={this.state.comments}
@@ -138,7 +213,7 @@ export default class App extends Component {
                 />
                 <FooterSection />
               </div>
-            </Route>*/}
+            </Route>
           </Switch>
         </div>
       </Router>
--- src/components/landing/3HeroCommentSection.js
+++ src/components/landing/3HeroCommentSection.js
@@ -1,81 +1,25 @@
 import React, {Component} from 'react';
-import Box from "3box";
+//import Box from "3box";
 import InputComments from './../../components/InputComments';
 import ProfileHover from "profile-hover";
 import { ClimbingBoxLoader } from "react-spinners";
 import Draggable from 'react-draggable';
-
-const getThreeBox = async address => {
-  const profile = await Box.getProfile(address);
-  return profile;
-};
 
 export default class AddApp extends Component {
   state = {
     thread: null
   };
 
-  async getAddressFromMetaMask() {
-    if (typeof window.ethereum == "undefined") {
-      this.setState({ needToAWeb3Browser: true });
-    } else {
-      window.ethereum.autoRefreshOnNetworkChange = false; //silences warning about no autofresh on network change
-      const accounts = await window.ethereum.enable();
-      this.setState({ accounts });
-    }
-  }
-
-
-  async getCommentsThread() {
-    const currentURL = window.location.href;
-    const cleanCurrentURL = currentURL.replace(/\//g, "_");
-    if (!this.props.threadComments) {
-      console.error("comments thread not in react state");
-      return;
-    }
-
-    // use this option, if your user has authenticated to their 3Box and space
-
-    const comments = await this.state.threadComments.getPosts();
-    this.setState({comments});
-
-    await this.props.threadComments.onUpdate(async()=> {
-      const comments = await this.props.threadComments.getPosts();
-      this.setState({comments});
-    })
-  }
-
-
   savePost = async formData => {
-    // should load metamask on save
+      // add the loggedin account to the form data to be saved
+      formData.account = this.props.accounts[0];
+      await this.props.thread.post(formData);
+      this.props.getCommentsThread();
+    };
 
-
-    await this.getAddressFromMetaMask();
-    if (this.state.accounts) {
-      const threeBoxProfile = await getThreeBox(this.state.accounts[0]);
-      this.setState({ threeBoxProfile });
-    }
-
-    const box = await Box.openBox(this.state.accounts[0], window.ethereum);
-    this.setState({ box });
-
-
-    const currentURL = window.location.href;
-    const cleanCurrentURL = currentURL.replace(/\//g, "_");
-    const space = await this.state.box.openSpace(cleanCurrentURL);
-    this.setState({ space });
-
-
-
-    // add the loggedin account to the form data to be saved
-    formData.account = this.state.accounts[0];
-    await this.props.thread.post(formData);
-    this.state.getCommentsThread();
-  };
-
-  state = {
-    show: false
-  };
+    state = {
+      show: false
+    };
 
   switchShowHide= () => {
           this.setState(prevState => {
@@ -85,88 +29,88 @@ export default class AddApp extends Component {
           })
       }
 
-  render() {
-    return (
-      <div className="container relative">
-        <div className="items-center text-center my-24 -mx-2">
-          <p className="mb-4 text-gray-700 ">
-            What do you think about this proposition?
-          </p>
-          <button onClick={this.switchShowHide} className="inline-block py-4 px-8 leading-none text-white bg-indigo-500 hover:bg-indigo-600 rounded shadow">
-            {this.state.show ? "cancel" : "Add your shitty opinion"}
-          </button>
-          <div className={`text-center absolute w-full ${this.state.show ? "block" : "hidden"}`}>
-            <div className="landingpage-comments-modal w-1/3 mx-auto">
+render() {
+     return (
+       <div className="container relative">
+         <div className="items-center text-center my-24 -mx-2">
+           <p className="mb-4 text-gray-700 ">
+             What do you think about this proposition?
+           </p>
+           <button onClick={this.switchShowHide} className="inline-block py-4 px-8 leading-none text-white bg-indigo-500 hover:bg-indigo-600 rounded shadow">
+             {this.state.show ? "cancel" : "Add your shitty opinion"}
+           </button>
+           <div className={`text-center absolute w-full ${this.state.show ? "block" : "hidden"}`}>
+             <div className="landingpage-comments-modal w-1/3 mx-auto">
 
-            {!this.props.thread && (
-              <div style={{ width: "100px", margin: "auto" }}>
-                <ClimbingBoxLoader color={"blue"} />
-              </div>
-            )}
-            {this.props.thread && <InputComments savePost={this.savePost} />}
+             {!this.props.thread && (
+               <div style={{ width: "100px", margin: "auto" }}>
+                 <ClimbingBoxLoader color={"blue"} />
+               </div>
+             )}
+             {this.props.thread && <InputComments savePost={this.savePost} />}
 
-          </div>
-        </div>
-      </div>
+           </div>
+         </div>
+       </div>
 
 
 
-      {/* show me all comments. could also be on the press of a button */}
-        <div className="">
-          {(!this.props.posts || this.props.posts.length < 1) && (
-            <div style={{ width: "60px", margin: "auto" }}>
-              <ClimbingBoxLoader color={"blue"} />
-            </div>
-          )}
-          {this.props.posts &&
-            this.props.posts.map((post, i) => {
-              return (
-                  <CommentCard
-                    post={post}
-                    key={i}
-                    threeBox={this.props.threeBox}
-                    space={this.props.space}
-                    box={this.props.box}
-                    usersAddress={this.props.usersAddress}
-                    i={i} />
-              );
-            })}
-        </div>
+       {/* show me all comments. could also be on the press of a button */}
+         <div className="">
+           {(!this.props.posts || this.props.posts.length < 1) && (
+             <div style={{ width: "60px", margin: "auto" }}>
+               <ClimbingBoxLoader color={"blue"} />
+             </div>
+           )}
+           {this.props.posts &&
+             this.props.posts.map((post, i) => {
+               return (
+                   <CommentCard
+                     post={post}
+                     key={i}
+                     threeBox={this.props.threeBox}
+                     space={this.props.space}
+                     box={this.props.box}
+                     usersAddress={this.props.usersAddress}
+                     i={i} />
+               );
+             })}
+         </div>
 
-    </div>
-    );
-  }
-}
+     </div>
+     );
+   }
+ }
 
-class CommentCard extends Component {
-  render(){
-    return (
-    <div className="h-0">
-      <Draggable defaultPosition={this.props.post.message.deltaPosition}>
-      <div className="comments-box-landing w-1/4 flex flex-col mx-auto items-center">
-        <div className="relative" style={{ padding: "20px" }}>
-          <p>
-            {this.props.post.message.comment ? this.props.post.message.comment : "unknown"}
-          </p>
-          <p>
-            {`${this.props.post.message.rating} stars` ? `${this.props.post.message.rating} stars` : "unknown"}
-          </p>
-          <p className="text-xs text-gray-500">
-            Position: x: {this.props.post.message.deltaPosition.x ? this.props.post.message.deltaPosition.x : "unknown"}, y: {this.props.post.message.deltaPosition.y ? this.props.post.message.deltaPosition.y : "unknown"}
-          </p>
-          {this.props.post.message.account && (
-            <div style={{ marginBottom: "10px" }}>
-              {/*<p className="text-xs">Submitted by</p>*/}
-              <ProfileHover
-                address={this.props.post.message.account}
-                style={{ width: "100%" }}
-                showName={true}
-              />
-            </div>
-          )}
-        </div>
-      </div>
-      </Draggable>
-    </div>)
-  }
-}
+ class CommentCard extends Component {
+   render(){
+     return (
+     <div className="h-0">
+       <Draggable defaultPosition={this.props.post.message.deltaPosition}>
+       <div className="comments-box-landing w-1/4 flex flex-col mx-auto items-center">
+         <div className="relative" style={{ padding: "20px" }}>
+           <p>
+             {this.props.post.message.comment ? this.props.post.message.comment : "unknown"}
+           </p>
+           <p>
+             {`${this.props.post.message.rating} stars` ? `${this.props.post.message.rating} stars` : "unknown"}
+           </p>
+           <p className="text-xs text-gray-500">
+             Position: x: {this.props.post.message.deltaPosition.x ? this.props.post.message.deltaPosition.x : "unknown"}, y: {this.props.post.message.deltaPosition.y ? this.props.post.message.deltaPosition.y : "unknown"}
+           </p>
+           {this.props.post.message.account && (
+             <div style={{ marginBottom: "10px" }}>
+               {/*<p className="text-xs">Submitted by</p>*/}
+               <ProfileHover
+                 address={this.props.post.message.account}
+                 style={{ width: "100%" }}
+                 showName={true}
+               />
+             </div>
+           )}
+         </div>
+       </div>
+       </Draggable>
+     </div>)
+   }
+ }
