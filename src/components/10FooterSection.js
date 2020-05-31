8588910136533cbfe992eb4a72ab00f9df220296
seichris <cerujurasa@gmail.com>
2020-05-31 22:13:48 +0200

    fix footer

--- src/components/10FooterSection.js
+++ src/components/10FooterSection.js
@@ -39,14 +39,13 @@ function ProductSection(props) {
         </div>*/}
       </div>
       <div className="mx-auto lg:mx-0 lg:ml-auto">
-        <a className="inline-block mt-0 text-blue-900 hover:text-blue-700" href="/">FAQ</a>
-        <a className="inline-block mt-0 ml-8 text-blue-900 hover:text-blue-700" href="/">Github</a>
-        <a className="inline-block mt-0 ml-8 text-blue-900 hover:text-blue-700" href="/">Discord</a>
+        {/*<a className="inline-block mt-0 text-blue-900 hover:text-blue-700" href="/">FAQ</a>*/}
+        <span>Built by <a className="inline-block underline mt-0 text-blue-900 hover:text-blue-700" rel="noopener noreferrer" target="_blank" href="https://twitter.com/chriscyber2000">Chris</a>, on <a rel="noopener noreferrer" target="_blank" className="inline-block underline mt-0 text-blue-900 hover:text-blue-700" href="https://github.com/seichris/xanadu">Github</a>.</span>
       </div>
-      <div className="flex justify-center mt-4 lg:mt-0 lg:ml-8">
+      {/*<div className="flex justify-center mt-4 lg:mt-0 lg:ml-8">
         <img className="w-6 h-6 mr-6" src={icon1} alt=""/>
         <img className="w-6 h-6" src={icon2} alt=""/>
-      </div>
+      </div>*/}
     </footer>
   );
 }
